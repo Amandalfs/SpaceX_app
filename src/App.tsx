48ea9f8e312886d29deaa451d2d159c9c444210e
Amandalfs <amandalfs57@gmail.com>
2023-08-01 18:53:16 -0300

    feat: adicionar o grafico no componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from './components/Header';
 import { StatsOfLaunches } from './components/StatsOfLaunches';
+import { StatsOfLaunchesByYears } from './components/StatsOfLaunchesByYears';
 
 function App() {
   return (
     <main className='flex flex-col h-screen bg-gray-150 '>
         <Header />
         <StatsOfLaunches />
+        <StatsOfLaunchesByYears />
     </main>
   )
 }
