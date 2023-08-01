1179b21c7951390a636a04609e8d39fc160d85f1
Amandalfs <amandalfs57@gmail.com>
2023-08-01 12:57:47 -0300

    chore: config colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        black: "#000000",
+        orange:{
+          50: "#CF6900", 
+          100:"#F57C00"
+        },
+        blue: "#1267FC",
+        white:"#FFFFFF",
+        gray: {
+          50: "#D9D9D9",
+          100:"#6C757D",
+          150:"#343A40",
+        },
+        "success": {
+          50: "#1FD636",
+          100:"#3F943E"
+        },
+        "failure": {
+          50: "#8B2A2D",
+          100:"#8D0606"
+        },
+      }
+    },
   },
   plugins: [],
 }
